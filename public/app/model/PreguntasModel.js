a13d1c238b559b28c885524efbecc4f2aa921823
Emmanuel Lucio Urbina <emmanuelluur@gmail.com>
2019-04-14 01:13:29 -0500

    Modelo Preguntas, template principal

---
+++ public/app/model/PreguntasModel.js
@@ -0,0 +1,7 @@
+const Preguntas = 
+[ 
+    { question: "Capital of Italy",    answer: "Rome" },
+    { question: "Capital of France",   answer: "Paris" },
+    { question: "Capital of Spain",    answer: "Madrid" },
+    { question: "Capital of Portugal", answer: "Lisbon" }
+];
